f1c077b92184e25539d3d05c37be6f40e4abc1e7
Marius Linge <atrius2k@gmail.com>
2015-05-07 00:59:27 +0200

    Removed screen-controllers and muted audio for all except the top left stream.

--- multistream.bat
+++ multistream.bat
@@ -1,8 +1,8 @@
 			:: Top Left
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-border --autofit=960x540 --geometry=0%%:0%%" twitch.tv/lirik source
+start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --autofit=960x540 --geometry=0%%:0%%" twitch.tv/lirik source
 			:: Top Right
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-border --autofit=960x540 --geometry=100%%:0%%" twitch.tv/lirik source
+start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --mute=yes --autofit=960x540 --geometry=100%%:0%%" twitch.tv/giantwaffle source
 			:: Bottom Left 
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-border --autofit=960x540 --geometry=0%%:100%%" twitch.tv/lirik source
+start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --mute=yes --autofit=960x540 --geometry=0%%:100%%" twitch.tv/shortyyguy source
 			:: Bottom Right
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-border --autofit=960x540 --geometry=100%%:100%%" twitch.tv/lirik source
+start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --mute=yes --autofit=960x540 --geometry=100%%:100%%" twitch.tv/itmejp source
