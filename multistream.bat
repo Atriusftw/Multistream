e87aca18dfbe60072e25c40bb9b5dabe8a4f6b5a
Marius Linge <atrius2k@gmail.com>
2015-05-07 18:26:09 +0200

    On top and monitor selection

--- multistream.bat
+++ multistream.bat
@@ -1,8 +1,9 @@
 			:: Top Left
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --autofit=960x540 --geometry=0%%:0%%" twitch.tv/lirik source
+start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen 1 --geometry=0%%:0%%" twitch.tv/lirik source
 			:: Top Right
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --mute=yes --autofit=960x540 --geometry=100%%:0%%" twitch.tv/giantwaffle source
+start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --mute=yes --autofit=960x540 --screen 1 --geometry=100%%:0%%" twitch.tv/giantwaffle source
 			:: Bottom Left 
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --mute=yes --autofit=960x540 --geometry=0%%:100%%" twitch.tv/shortyyguy source
+start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --mute=yes --autofit=960x540 --screen 1 --geometry=0%%:100%%" twitch.tv/cincinbear source
 			:: Bottom Right
-start cmd /k livestreamer --player="C:/Users/Marius/Desktop/mpv/mpv.exe --no-osc --no-border --mute=yes --autofit=960x540 --geometry=100%%:100%%" twitch.tv/itmejp source
+start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --mute=yes --autofit=960x540 --screen 1 --geometry=100%%:100%%" twitch.tv/itmejp source
+EXIT /B
