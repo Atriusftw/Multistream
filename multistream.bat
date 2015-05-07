df77e83fe1f769f99896e9a2407adabf9cd95c7d
Marius Linge <atrius2k@gmail.com>
2015-05-08 00:41:15 +0200

    Dummy GUI asking the user what streams to launch.

--- multistream.bat
+++ multistream.bat
@@ -1,9 +1,21 @@
-			:: Top Left
-start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen 1 --geometry=0%%:0%%" twitch.tv/lirik source
-			:: Top Right
-start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --mute=yes --autofit=960x540 --screen 1 --geometry=100%%:0%%" twitch.tv/giantwaffle source
-			:: Bottom Left 
-start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --mute=yes --autofit=960x540 --screen 1 --geometry=0%%:100%%" twitch.tv/cincinbear source
-			:: Bottom Right
-start livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --mute=yes --autofit=960x540 --screen 1 --geometry=100%%:100%%" twitch.tv/itmejp source
+@echo off
+SetLocal
+ECHO Multistream v0.2
+ECHO.
+ECHO Currently only supports 1920x1080 and asks for four streams
+ECHO.
+ECHO.
+
+set /P stream_username1=Type in stream name 1: %=%
+set /P stream_username2=Type in stream name 2: %=%
+set /P stream_username3=Type in stream name 3: %=%
+set /P stream_username4=Type in stream name 4: %=%
+
+  echo Opening streams %stream_username1%, %stream_username2%, %stream_username3% and %stream_username4%
+    start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen 1 --geometry=0%%:0%%" http://twitch.tv/%stream_username1% best --loglevel none
+  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen 1 --geometry=100%%:0%%" http://twitch.tv/%stream_username2% best --loglevel none
+  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen 1 --geometry=0%%:100%%" http://twitch.tv/%stream_username3% best --loglevel none
+  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen 1 --geometry=100%%:100%%" http://twitch.tv/%stream_username4% best --loglevel none
 EXIT /B
+	)
+EndLocal	
