c005810b42e351be35f477856556f548a44dd240
Marius Linge <atrius2k@gmail.com>
2015-05-08 02:32:19 +0200

    Mute all streams by default

--- multistream.bat
+++ multistream.bat
@@ -14,10 +14,10 @@ set /P stream_username3=Type in stream name 3: %=%
 set /P stream_username4=Type in stream name 4: %=%
 
   echo Opening streams %stream_username1%, %stream_username2%, %stream_username3% and %stream_username4%
-    start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=0%%:0%%" http://twitch.tv/%stream_username1% best --loglevel none
-  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=100%%:0%%" http://twitch.tv/%stream_username2% best --loglevel none
-  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=0%%:100%%" http://twitch.tv/%stream_username3% best --loglevel none
-  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=100%%:100%%" http://twitch.tv/%stream_username4% best --loglevel none
+    start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --mute=yes --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=0%%:0%%" http://twitch.tv/%stream_username1% best --loglevel none
+  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --mute=yes --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=100%%:0%%" http://twitch.tv/%stream_username2% best --loglevel none
+  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --mute=yes --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=0%%:100%%" http://twitch.tv/%stream_username3% best --loglevel none
+  	start /B livestreamer --player="C:/Program Files/mpv/mpv.exe --ontop --mute=yes --no-osc --no-border --autofit=960x540 --screen %monitor% --geometry=100%%:100%%" http://twitch.tv/%stream_username4% best --loglevel none
 EXIT /B
 	)
 EndLocal	
